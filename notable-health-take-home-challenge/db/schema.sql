0a704e3d03021887ca8196ae69dfcb553e1661ec
jimmylu1 <jimmy88lu@gmail.com>
2019-12-18 13:51:40 -0800

    seed database with fake data

--- notable-health-take-home-challenge/db/schema.sql
+++ notable-health-take-home-challenge/db/schema.sql
@@ -36,3 +36,26 @@ CREATE TABLE appointments (
 
 ALTER TABLE appointments ADD FOREIGN KEY (doctor) REFERENCES doctors (id);
 ALTER TABLE appointments ADD FOREIGN KEY (patient) REFERENCES patients (id);
+
+INSERT INTO doctors(firstname, lastname, email) VALUES('Jim', 'Halpert', 'halpert@notablehealth.com');
+INSERT INTO doctors(firstname, lastname, email) VALUES('Dwight', 'Schrute', 'schrute@notablehealth.com');
+INSERT INTO doctors(firstname, lastname, email) VALUES('Pam', 'Beasely', 'beasely@notablehealth.com');
+INSERT INTO doctors(firstname, lastname, email) VALUES('Stanley', 'Hudson', 'hudson@notablehealth.com');
+INSERT INTO doctors(firstname, lastname, email) VALUES('Jan', 'Levinson', 'levinson@notablehealth.com');
+
+INSERT INTO patients(firstname, lastname, email) VALUES('Michael', 'Scott', 'scott@office.com');
+INSERT INTO patients(firstname, lastname, email) VALUES('Angela', 'Howard', 'howard@office.com');
+INSERT INTO patients(firstname, lastname, email) VALUES('Toby', 'Flenderson', 'flenderson@office.com');
+INSERT INTO patients(firstname, lastname, email) VALUES('Kevin', 'Malone', 'malone@office.com');
+INSERT INTO patients(firstname, lastname, email) VALUES('Phyllis', 'Vance', 'vance@office.com');
+
+INSERT INTO appointments(doctor, patient, date, time, kind) VALUES(2, 2, '2019-12-18', '10:00AM', 'Follow-up');
+INSERT INTO appointments(doctor, patient, date, time, kind) VALUES(5, 2, '2019-12-18', '9:00AM', 'New Patient');
+INSERT INTO appointments(doctor, patient, date, time, kind) VALUES(4, 1, '2019-12-18', '9:30AM', 'New Patient');
+INSERT INTO appointments(doctor, patient, date, time, kind) VALUES(2, 3, '2019-12-19', '11:00AM', 'Follow-up');
+INSERT INTO appointments(doctor, patient, date, time, kind) VALUES(3, 5, '2019-12-18', '12:30PM', 'New Patient');
+INSERT INTO appointments(doctor, patient, date, time, kind) VALUES(1, 4, '2019-12-20', '10:30AM', 'New Patient');
+INSERT INTO appointments(doctor, patient, date, time, kind) VALUES(3, 1, '2019-12-21', '2:30PM', 'Follow-up');
+INSERT INTO appointments(doctor, patient, date, time, kind) VALUES(3, 5, '2019-12-21', '2:30PM', 'Follow-up');
+INSERT INTO appointments(doctor, patient, date, time, kind) VALUES(3, 5, '2019-12-21', '2:30PM', 'Follow-up');
+
